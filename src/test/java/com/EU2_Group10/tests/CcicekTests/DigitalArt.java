a3a66743df1e34f288315a3bf37c8ffb9b7561c7
memocpr <memocpr35@gmail.com>
2020-05-22 23:52:58 +0200

    add DigitalArt

--- src/test/java/com/EU2_Group10/tests/CcicekTests/DigitalArt.java
+++ src/test/java/com/EU2_Group10/tests/CcicekTests/DigitalArt.java
@@ -3,6 +3,6 @@ package com.EU2_Group10.tests.CcicekTests;
 public class DigitalArt {
 
     public static void main(String[] args) {
-        System.out.println("Chanlenging Everthing");
+        System.out.println("Chalqlenging Everthing");
     }
 }
