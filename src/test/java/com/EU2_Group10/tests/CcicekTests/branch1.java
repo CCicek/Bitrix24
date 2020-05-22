a435cc8a3f20c25e40ee59c23e119e75f8ec2ead
CCicek <muhammetcicekster@gmail.com>
2020-05-22 13:56:26 +0000

    branch1

---
+++ src/test/java/com/EU2_Group10/tests/CcicekTests/branch1.java
@@ -0,0 +1,11 @@
+package com.EU2_Group10.tests.CcicekTests;
+
+import com.google.gson.internal.bind.util.ISO8601Utils;
+
+public class branch1 {
+
+    public static void main(String[] args) {
+        System.out.println("open branch...");
+    }
+
+}
